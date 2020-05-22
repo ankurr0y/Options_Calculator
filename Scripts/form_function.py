4cfd6e819af5bcb9b32e523d6a2ad9c24fd2a598
ankurr0y <ankur_roy@outlook.com>
2020-05-22 07:33:00 +0545

    That's it for UI at the moment

--- Scripts/form_function.py
+++ Scripts/form_function.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import FileField,TextField, IntegerField, TextAreaField, SubmitField, RadioField, SelectField, validators, ValidationError,FloatField
+from wtforms import FileField,TextAreaField, SubmitField,SelectField,validators, ValidationError,FloatField
 
 class InfoForm(FlaskForm):
-    currentStockPrice=FloatField('Enter the current price of the stock',[validators.Required('Compulsory Field')])
+    currentStockPrice=FloatField('Enter the current price of the stock:',[validators.Required('Compulsory Field')])
     riskFreeRate=FloatField('Enter current risk free rate:',[validators.Required('Compulsory Field')])
     exercisePrice=FloatField('Enter exercise price:',[validators.Required('Compulsory Field')])
     file=FileField('Enter the csv file with stock information:',[validators.Required('Compulsory Field')])
