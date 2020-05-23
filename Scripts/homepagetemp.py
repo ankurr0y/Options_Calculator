c31a5618ba38cc55cc4990403bd321ae42dea733
ankurr0y <ankur_roy@outlook.com>
2020-05-23 21:19:01 +0545

    Major changes committed

--- Scripts/homepagetemp.py
+++ Scripts/homepagetemp.py
@@ -4,9 +4,18 @@ app=Flask(__name__)
 app.secret_key = 'development key'
 
 @app.route('/',methods=['GET','POST'])
+@app.route('/home/',methods=['GET','POST'])
+def upload():
+   return render_template('homepage.html')
+
+@app.route('/next/',methods=['GET','POST'])
 def index():
-    form=InfoForm()
-    return render_template('index.html',form=form)
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save('templates/'+f.filename)
+        print(f.filename)
+        form=InfoForm()
+        return render_template('index.html',form=form)
 
     
 if __name__ == '__main__':
