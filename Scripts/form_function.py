012f5bafa362e419ef0e8fdaacac86919c32a198
ankurr0y <ankur_roy@outlook.com>
2020-05-24 10:56:50 +0545

    Some progress

--- Scripts/form_function.py
+++ Scripts/form_function.py
@@ -5,5 +5,4 @@ class InfoForm(FlaskForm):
     currentStockPrice=FloatField('Enter the current price of the stock:',[validators.Required('Compulsory Field')])
     riskFreeRate=FloatField('Enter current risk free rate:',[validators.Required('Compulsory Field')])
     exercisePrice=FloatField('Enter exercise price:',[validators.Required('Compulsory Field')])
-    #file=FileField('Enter the csv file with stock information:',[validators.Required('Compulsory Field')])
     submit=SubmitField('Input this information')
--- Scripts/homepagetemp.py
+++ Scripts/homepagetemp.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,url_for,flash,request
 from form_function import InfoForm
 app=Flask(__name__)
 app.secret_key = 'development key'
-
+filename=''
 @app.route('/',methods=['GET','POST'])
 @app.route('/home/',methods=['GET','POST'])
 def upload():
@@ -13,10 +13,13 @@ def index():
     if request.method == 'POST':
         f = request.files['file']
         f.save('templates/'+f.filename)
-        print(f.filename)
+        print(type(f.filename))
+        filename=f.filename
+        print(filename)
         form=InfoForm()
-        return render_template('index.html',form=form)
+        return render_template('index.html',form=form,filename=filename)
 
-    
+def getFileName():
+   return filename
 if __name__ == '__main__':
    app.run()
