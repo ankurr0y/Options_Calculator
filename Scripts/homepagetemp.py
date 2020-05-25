de5a2c6a0a5c0a539bbd27b9175775b8788ff7af
ankurr0y <ankur_roy@outlook.com>
2020-05-25 10:18:21 +0545

    Slight change of plans

--- Scripts/homepagetemp.py
+++ Scripts/homepagetemp.py
@@ -5,26 +5,30 @@ import pandas as pd
 app=Flask(__name__)
 app.secret_key = 'development key'
 filename=''
-def extract(filename):
+'''def extract(filename):
    print(filename)
    extension='templates//'+filename
    df=pd.read_csv(extension)
    high=df['High']
-   return high
+   return high'''
 @app.route('/',methods=['GET','POST'])
 @app.route('/home/',methods=['GET','POST'])
-def upload():
-   return render_template('homepage.html')
+def homepage():
+   form=InfoForm()
+   return render_template('Options_Calculator.html',form=form)
+'''def upload():
+   return render_template('homepage.html')'''
 
 @app.route('/next/',methods=['GET','POST'])
 def index():
     if request.method == 'POST':
-        f = request.files['file']
+        '''f = request.files['file']
         f.save('templates/'+f.filename)
         print(type(f.filename))
         filename=f.filename
         form=InfoForm()
-        return render_template('index.html',form=form,filename=extract(filename))
+        return render_template('index.html',form=form,filename=extract(filename))'''
+        return render_template(url_for('youtube.com'))
 
 if __name__ == '__main__':
    app.run()
